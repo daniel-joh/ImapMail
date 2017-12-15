b96e15eb75ff4580f1c5b9809b4f729610972508
daniel-joh <danielj@comhem.se>
2017-12-15 02:35:33 +0100

    Adds MailHandler class & UI changes

---
+++ ImapMail/MailHandler.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MailKit;
+using MimeKit;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+
+namespace ImapMail
+{
+    public class MailHandler
+    {
+        private static ImapClient client;
+        private static string user = "USER";
+        private static string password = "PASSWORD";
+
+
+        public static ObservableCollection<MessageSummary> GetMailSummaries()
+        {
+
+            ObservableCollection<MessageSummary> summaryList;
+
+            using (client = new ImapClient())
+            {
+                //client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client.Connect("imap.gmail.com", 993, true);
+
+                //client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                client.Authenticate(user, password);
+
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadOnly);
+
+                //Debug.WriteLine("Total messages: {0}", inbox.Count);
+
+                summaryList = new ObservableCollection<MessageSummary>();
+
+                var summaryItems = inbox.Fetch(0, -1, MessageSummaryItems.Envelope | MessageSummaryItems.UniqueId).ToList();
+
+                //Sorts messages in reverse arrival order
+                var orderBy = new[] { MailKit.Search.OrderBy.ReverseArrival };
+                MessageSorter.Sort(summaryItems, orderBy);
+
+                foreach (MessageSummary summary in summaryItems)
+                {
+                    summaryList.Add(summary);
+
+                }
+
+                //client.Disconnect(true);
+
+            }
+
+            return summaryList;
+        }
+
+        public static MimeMessage GetSpecificMail(UniqueId uid)
+        {
+            MimeMessage returnMail;
+
+            using (client = new ImapClient())
+            {
+                //client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client.Connect("imap.gmail.com", 993, true);
+
+                //client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                client.Authenticate(user, password);
+
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadOnly);
+
+                returnMail = inbox.GetMessage(uid);
+
+                //client.Disconnect(true);
+            }
+
+            return returnMail;
+
+        }
+
+        
+    }
+}
+
+
+
--- ImapMail/MainPage.xaml.cs
+++ ImapMail/MainPage.xaml.cs
@@ -12,6 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using MailKit;
+using System.Collections.ObjectModel;
+using MimeKit;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +26,47 @@ namespace ImapMail
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        public ObservableCollection<MessageSummary> SummaryList { get; set; }
+
+        public ObservableCollection<MimeMessage> MailList { get; set; }
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            SummaryList = MailHandler.GetMailSummaries();
+            MimeMessage mail = MailHandler.GetSpecificMail(SummaryList[0].UniqueId);
+
+            if (mail.HtmlBody != null)
+            {
+                webView.NavigateToString(mail.HtmlBody);
+            }
+            else if (mail.TextBody != null)
+            {
+                webView.NavigateToString(mail.TextBody);
+            }
+
+        }
+
+        private void MailListView_ItemClicked(object sender, ItemClickEventArgs e)
+        {
+            MessageSummary msg = (MessageSummary)e.ClickedItem;
+
+            Debug.WriteLine("Unique id: " + msg.UniqueId);
+
+            MimeMessage mail = MailHandler.GetSpecificMail(msg.UniqueId);
+
+            if (mail.HtmlBody != null)
+            {
+                webView.NavigateToString(mail.HtmlBody);
+            }
+            else if (mail.TextBody != null)
+            {
+                webView.NavigateToString(mail.TextBody);
+            }
+
+
         }
     }
 }
